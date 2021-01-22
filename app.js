6d89ad2194eb9b10d6b158e92dd35d56e392cdd0
Timits <will4f21@yahoo.com>
2021-01-22 03:04:07 -0800

    calculate tipfunc w/ bill*tip percntg divide by ppl display innrhtml

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 // calculate tip
 function calculateTip() {
-    var billAmt = document.getElementById('billamt').value;
-    var serviceQual = document.getElementById('serviceQual').value;
-    var numOfPeople = document.getElementById('pplsplit').value;
+    // capture all three input fields or options 
+    let totalBill = document.getElementById('totalbill').value;
+    let gratuity = document.getElementById('gratuity').value;
+    let totalPeople = document.getElementById('pplsplit').value;
 
-    // validate input
-    if(billAmt === "" || serviceQual == 0) {
+    // validate input // there has to be an amount entered in both of these fields
+    if(totalBill === "" || gratuity == 0) {
         alert("Please enter values");
         return;
     }
-    
 
-    // check to see if this input is empty or less than equal to 1
-    if(numOfPeople === "" || numOfPeople == 0) {
-        numOfPeople = 1;
+     // function to calculate tip then divide by individuals
+     let total = (totalBill * gratuity) / totalPeople;
+     // round to two decimal places
+     total = Math.round(total * 100) / 100;
+     // next line allows us to always have two digits after decimal point
+     total = total.toFixed(2);
+     // Display the tip
+     document.getElementById('totaltip').style.display = 'block';
+     document.getElementById('tip').innerHTML = total;
+
+    // check to see if this input is empty or less than equal to 1 // people input can be empty
+    if(totalPeople === "" || totalPeople <= 1) {
+        totalPeople = 1;
         document.getElementById('each').style.display = " none";
     } else {
-        document.getElementById("each").style.display = "block";
+        document.getElementById('each').style.display = "block";
     }
 
-
 };
+
+ // Hide the tip amount when load
+ document.getElementById('totaltip').style.display = 'none';
+ document.getElementById('each').style.display = 'none';
+
+ // click to call  the function
+ document.getElementById('calculate').onclick = function() {
+     calculateTip();
+ }
 
 
 
